ecb508a19c677d583082f402763911d30291cf03
Tom Bonnike <bonniketom@gmail.com>
2017-01-02 02:03:12 +0100

    Moved autoprefixer to jekyll plugin and added bootstrap uncss script

---
+++ scripts/uncss.js
@@ -0,0 +1,29 @@
+var uncss = require('uncss');
+var glob = require('glob');
+var fs = require('fs');
+
+var sourceStylesheet = 'assets/sass/vendors/_bootstrap.scss';
+var resultStylesheet = 'assets/sass/vendors/_bootstrap.un.scss';
+
+var bootstrapUncss = function () {
+  var css = fs.readFileSync(sourceStylesheet, 'utf8');
+
+  glob('_site/**/*.html', function (err, files) {
+    if (err) {
+      console.log(err);
+    }
+
+    uncss(files, {
+      raw: css,
+      ignoreSheets:[/\/css\//]
+    }, function (err, output) {
+      if (err) {
+        console.log(err);
+      }
+
+      fs.writeFileSync(resultStylesheet, output);
+    });
+  });
+};
+
+bootstrapUncss();
